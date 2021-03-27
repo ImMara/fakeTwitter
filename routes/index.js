64ac3cc1173e79964d3a5c420dd5016da4584553
Mara <alaneraerts@live.be>
2021-03-27 11:56:03 +0100

    add tweet form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+router.get('/tweet/new',(req,res) => res.render('tweets/tweet-form'));
+
 router.get('/', (req, res) => {
   res.render('tweets/tweet-list');
 })
