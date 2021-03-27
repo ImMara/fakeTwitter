8bc94f1de137c8ddc1e25e7dfd76acb8a7c01777
Mara <alaneraerts@live.be>
2021-03-27 13:23:29 +0100

    create api

--- database/models/tweet.model.js
+++ database/models/tweet.model.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const tweetSchema = schema({
-    content:{ type:String , maxLength:140, minLength:1 , required: true }
+    content:{ type:String ,
+        maxLength:[140 , 'Tweet trop long'],
+        minLength:[1 , 'Tweet trop court'] ,
+        required: [true , 'Champ requis']
+    }
 })
 
 const Tweet = mongoose.model('tweet', tweetSchema)
