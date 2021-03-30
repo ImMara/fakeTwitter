9a0053e72c29dd72dce9612a98962e343d62085d
Mara <alaneraerts@live.be>
2021-03-31 01:23:28 +0200

    queries and error handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('./database');
 const app = express();
 const port = process.env.PORT || 3000;
 const index = require('./routes');
+const errorHandler = require('errorhandler');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -14,5 +15,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(index);
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(errorHandler)
+}else{
+    app.use((err , req , res , next) =>{
+        const code = err.code || 500;
+        res.status(code).json({
+            code:code,
+            message:code === 500 ? null : err.message
+        });
+    })
+}
 
 app.listen(port);
