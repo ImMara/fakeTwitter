6ff708ddd0214b001ad44a86a622f5c311048918
Mara <alaneraerts@live.be>
2021-03-31 00:57:34 +0200

    Refacto controllers

---
+++ routes/tweets.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { tweetList , tweetNew , tweetCreate } = require('../controllers/tweets.controller')
+
+router.get('/',  tweetList );
+router.get('/new', tweetNew);
+router.post('/', tweetCreate);
+
+module.exports = router;
