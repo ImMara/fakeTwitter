7b3239d8013ff9f1854dd70a56fa357b0fcfcba1
Mara <alaneraerts@live.be>
2021-04-04 01:20:32 +0200

    auth routes passport

---
+++ controllers/auth.controller.js
@@ -0,0 +1,10 @@
+exports.signinForm = ( req , res , next ) => {
+    res.end();
+}
+exports.signin = ( req , res , next ) => {
+    res.end();
+}
+
+exports.signout = ( req , res , next ) =>{
+    res.end();
+}
